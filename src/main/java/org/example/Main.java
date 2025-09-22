2ca4db24575224971ddbd13139cd4cc491f13efe
HiDDeN <denis.naydin228@gmail.com>
2025-09-22 17:38:28 +0500

    Solved task for 3 variant

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int weight = sc.nextInt();
+        double height = (double)sc.nextInt() / 100;
+        double bmi = (double) weight / (height * height);
+        if (bmi < 18.5){
+            System.out.print("Недостаточный вес");
+        } else if (bmi < 25.0) {
+            System.out.print("Норма");
+        } else if (bmi < 30.0) {
+            System.out.print("Избыточный вес");
+        }
+        else{
+            System.out.print("Ожирение");
+        }
     }
 }
