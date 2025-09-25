f8a2422ad699ec2b45e498bf4944134ebb986cd2
Pavloxes <pavloxes@gmail.com>
2025-09-25 20:44:51 +0500

    Pak

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        long ms_in_day = 1000 * 60 * 60 * 24, cur_time = System.currentTimeMillis(),
+        ms_in_cur_day = cur_time % ms_in_day, 
+        hrs_in_cur_day = ms_in_cur_day / (1000 * 60 * 60),
+        minutes_in_cur_day = (ms_in_cur_day / (1000 * 60)) % 60,
+        seconds_in_cur_day = (ms_in_cur_day / 1000) % 60;
+        System.out.println(hrs_in_cur_day + ":" + minutes_in_cur_day + ":" + seconds_in_cur_day + " GMT");
     }
 }
