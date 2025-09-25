24f2c3f2c6d51f7c940a4996f6f4740ef72d9b2d
OpiumCatt <denisvertiprakhov@mail.ru>
2025-09-25 23:37:04 +0500

    Написали решение для второй задачи

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
+import java.util.Scanner;
+import java.math.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Введите ежемесячную процентную ставку: ");
+        double rate = scanner.nextDouble();
+        rate /= 100.0;
+        System.out.print("Введите сумму кредитка: ");
+        double sum = scanner.nextDouble();
+        System.out.print("Введите срок кредита (целое количество лет): ");
+        int len_years = scanner.nextInt();
+        int len_months = len_years * 12;
+        scanner.close();
+        
+        double monthly_payment = sum * rate / (1.0 - 1.0 / Math.pow(1.0 + rate, len_months));
+        double total_cost = monthly_payment * len_months;
+        
+        System.out.println("Ежемесячный платёж: " + String.valueOf(monthly_payment));
+        System.out.println("Стоимость кредита: " + String.valueOf(total_cost));
     }
 }
