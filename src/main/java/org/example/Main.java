26a39a08309f2d3d6100aa56b956835b5f96e235
Burkov Denis <>
2025-09-26 10:40:15 +0500

    Решение задания №1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+		Long x = System.currentTimeMillis() / 1000;
+		x = x % (24 * 60 * 60);
+		Long h = (x) / (60 * 60);
+		Long m = (x - h * 60 * 60) / (60);
+		Long s = (x - h * 60 * 60 - m * 60) / (60);
+        System.out.printf("%02d:%02d:%02d GMT\n", h, m, s);
     }
 }
